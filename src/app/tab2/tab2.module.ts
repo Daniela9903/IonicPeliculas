993565cf158f64183528fd821ba95fd5538e7508
Daniela9903 <rosas.tome@gmail.com>
2022-03-03 16:04:52 -0600

    android

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -14,7 +14,7 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule,RouterModule
+    Tab2PageRoutingModule
   ],
   declarations: [Tab2Page]
 })
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { PeliculasService } from 'src/app/services/peliculas.service';
+import { FormBuilder } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page{
 
-  constructor() {}
+  formulario = this.formBuilder.group({ //inicializar en 0 
+    nombrePelicula:'',
+    anioEstreno:'',
+    genero:'',
+    cal:'',
+    sinopsis:''
+  });
 
+
+  constructor(private peliculasService:PeliculasService , private formBuilder:FormBuilder) {}
+
+
+  ngOnInit(): void {
+
+    
+  }
+
+  Guardar(){
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Pelicula agragada',
+      showConfirmButton: false,
+      timer: 1500
+    })
+    console.log(this.formulario.value)
+    this.peliculasService.creatPelicula(this.formulario.value).subscribe((res:any) =>{
+      console.log(res)
+    })
+    
+  }
 }
